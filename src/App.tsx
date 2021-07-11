9b85bca0025262e4321583b38a247445c9faed26
luisp <luis.p.durao@hotmail.com>
2021-07-11 14:14:08 +0100

    3 - Memoize

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useMemo, useState } from 'react';
 import People from './People';
+
+/* const peopleList = {
+  "luis-cirne": {
+    name: 'Luis Cirne',
+    initialAge: 25
+  },
+  "fredrik-gelotte": {
+    name: 'Fredrik Gelotte',
+    initialAge: 33
+  },
+  "alejandro-andersson": {
+    name: 'Alejandro Andersson',
+    initialAge: 28
+  }
+} as const; */
 
 function App() {
   console.log("rendering app")
@@ -10,23 +25,25 @@ function App() {
     setTeamName(prompt('New team name') || teamName)
   }
 
+  const peopleList = useMemo(() => ({
+    "luis-cirne": {
+      name: 'Luis Cirne',
+      initialAge: 25
+    },
+    "fredrik-gelotte": {
+      name: 'Fredrik Gelotte',
+      initialAge: 33
+    },
+    "alejandro-andersson": {
+      name: 'Alejandro Andersson',
+      initialAge: 28
+    }
+  }), [])
+
   return (
     <div>
       <h1 onClick={onTeamNameClick}>{teamName}</h1>
-      <People people={{
-        "luis-cirne": {
-          name: 'Luis Cirne',
-          initialAge: 25
-        },
-        "fredrik-gelotte": {
-          name: 'Fredrik Gelotte',
-          initialAge: 33
-        },
-        "alejandro-andersson": {
-          name: 'Alejandro Andersson',
-          initialAge: 28
-        }
-      }}/>
+      <People people={peopleList}/>
     </div>
   );
 }
--- src/Person.tsx
+++ src/Person.tsx
@@ -1,4 +1,4 @@
-import { memo, useState } from 'react';
+import { memo, useCallback, useState } from 'react';
 import Age from "./Age"
 import Name from "./Name"
 
@@ -16,11 +16,14 @@ export default memo(function Person({id, data: {name, initialAge}}: PersonProps)
     const [goals, setGoals] = useState(0);
     const [age, setAge] = useState(initialAge);
 
+    //const incrementAge = useMemo(() => () => {setAge(a => a+1)}, [])
+    const incrementAge = useCallback(() => {setAge(a => a+1)}, [])
+
     return (
         <div>
             <p>ID: {id}</p>
             <Name name={name}/>
-            <Age age={age} increment={() => {setAge(a => a+1)}}/>
+            <Age age={age} increment={incrementAge}/>
             <p onClick={() => {setGoals(d => d+1)}}>Goals: {goals}</p>
             <hr/>
         </div>
